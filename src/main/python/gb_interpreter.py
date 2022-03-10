bde855b10fa9dc8d4bb26a1448666d630f34189e
Michael Conrad <m.conrad.202@gmail.com>
2022-03-10 00:41:43 -0500

    sync

--- src/main/python/gb_interpreter.py
+++ src/main/python/gb_interpreter.py
@@ -3,22 +3,28 @@ from lark import Token
 from lark import Tree
 
 
-def unescape(cls, text: str) -> str:
+def unescape(text: str) -> str:
     return text.encode('utf-8').decode('unicode-escape')
 
 
-def escape(cls, text: str) -> str:
+def escape(text: str) -> str:
     return text.encode('unicode-escape').decode('utf-8')
 
 
 class GamebookInterpreter(lark.visitors.Interpreter):
+    """
+    Two main phases:
+     phase one (at init), store parsed tree
+     phase two (on run), process stored and parsed tree as instructions
+    """
 
     globals: dict = dict()
     section_state: dict[str, dict[str, any]] = dict()
     section_stack: list[str] = list()
+    section_commands: dict[str, list[str]] = dict()
 
     def __init__(self):
-        self.section_stack.append("book")
+        self.section_stack.append("Game Book")
         self.section_state[self.current_section] = dict()
 
     @property
@@ -29,18 +35,19 @@ class GamebookInterpreter(lark.visitors.Interpreter):
         self.visit_children(tree)
 
     def section(self, tree: Tree):
-        self.visit_children(tree)
-        self.section_stack.pop()
+        return self.visit_children(tree)
+
+    def section_header(self, tree: Tree):
+        return self.visit_children(tree)
 
     def section_name(self, tree: Tree):
         for child in tree.children:
-            if isinstance(child, Tree):
-                self.visit(child)
-            elif isinstance(child, Token):
+            if isinstance(child, Token):
+                if child.value in self.section_state:
+                    raise SyntaxError(f"Section {child.value} previously defined.")
                 self.section_stack.append(child.value)
-                if self.current_section not in self.section_state:
-                    self.section_state[self.current_section] = dict()
-                print(f"=== {child.value}")
+                self.section_state[self.current_section] = dict()
+                return str(child.value)
 
     def metadata_namevalue(self, tree: Tree):
         child: Token | Tree
@@ -53,19 +60,22 @@ class GamebookInterpreter(lark.visitors.Interpreter):
         metadata: dict[str, list[str]] = state["metadata"]
 
         for child in tree.children:
-            if isinstance(child, Tree):
-                self.visit(child)
-            elif isinstance(child, Token):
+            if isinstance(child, Token):
                 if child.type == "NAME":
                     tag = child.value.strip()
                 elif child.type == "REMAINING_LINE":
                     value = child.value.strip()
 
+        if not tag:
+            print("Bad Metadata Entry")
+            return None
         if tag not in metadata:
             metadata[tag] = list()
         metadata[tag].append(value)
         if tag.lower() == "library":
             value = value.strip()
-            exec(f"import {value}", self.globals)
-
+            if not value:
+                print("Bad Library Entry Skipped")
+                return
+            return f"import {value}"
 
